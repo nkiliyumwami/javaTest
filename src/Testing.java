dd684742acee621bee7a6b8ac0da24a95704510d
emmanuelnkiriyumwami <emmanuelnkiriyumwami@localhost>
2022-05-03 22:35:13 -0400

    First Commit

---
+++ src/Testing.java
@@ -0,0 +1,28 @@
+
+
+public class Testing {
+
+	public static void main(String[] args) {
+
+		/* for loop */
+		
+		for(int i = 0; i <=10; i++) {
+			System.out.println(i);
+		}
+		
+		/* while loop */
+		/* int a = 0; */
+		
+		/* String text = " "; */
+		
+		int a = 0;
+		while(a < 5) {
+			 String text = " "; 
+			 text += "The number is: " + a;
+			 a++;
+			
+			 System.out.println(text);
+		}
+	}
+
+}

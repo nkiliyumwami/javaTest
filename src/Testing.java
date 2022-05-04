13390035bf01ea0d86bd14a5bb71f0876a582d21
Emmanuel Nkiriyumwami <nkiliyumwami@gmail.com>
2022-05-04 15:30:35 -0400

    Conditionnal and Loops

--- src/Testing.java
+++ src/Testing.java
@@ -1,4 +1,4 @@
-
+import org.w3c.dom.Text;
 
 public class Testing {
 
@@ -7,23 +7,49 @@ public class Testing {
 		/* for loop */
 		
 		for(int i = 0; i <=10; i++) {
-			System.out.println(i);
+			String text = "The number is: ";
+			System.out.println(text + i);
 		}
 		
 		/* while loop */
-		/* int a = 0; */
-		
-		/* String text = " "; */
 		
 		int a = 0;
-		while(a < 5) {
-			 String text = " "; 
-			 text += "The number is: " + a;
-			 a++;
-			
-			 System.out.println(text);
-			 
-			 System.out.println("This is to test git integration!");
+		
+		while(a <=5) {
+			String text = "Hello: ";
+			System.out.println(text + a);
+			a++;
+		}
+		
+		
+		/* conditional statements */
+		
+		int age = 40;
+		char sex = 'M';
+		double weight = 51.5;
+		
+		
+		if(age >=18 && weight <=50) {
+			System.out.println("You are eligible");
+		} else {
+			System.out.println("You will have to wait until you became 18 years and must be under 50kg by that time.");
+		}
+		
+		/* switch statements */
+		
+		char grade = 'A';
+		
+		switch (grade) {
+		case 'A':
+			System.out.println("100%-90%");
+			break;
+		case 'B': 
+			System.out.println("80%");
+			break;
+
+		default:
+			System.out.println("Failed");
+			break;
 		}
 	}
 
